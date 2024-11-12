444926cbae186e57332d4361a497081a35160f4c
yechaofan <1319646692@qq.com>
2024-11-12 13:06:56 +0800

    feat: 循环

--- 应用代码/09.循环/index.js
+++ 应用代码/09.循环/index.js
@@ -1,25 +1,30 @@
-/* for...in和for...of的区别 */
-function fn1() {
-  const obj = {
-    a: 1,
-    b: 2,
+/* for...in循环对象上的可枚举属性，包括原型上的，但没有顺序 */
+function forin() {
+  const object1 = Object.create({
     c: 3,
-  };
+  });
+  object1.a = 1;
+  Object.defineProperty(object1, "b", {
+    value: 2,
+    enumerable: true,
+  });
 
-  const map = new Map([
-    [1, "value1"],
-    [2, "value2"],
-    [3, "value3"],
-  ]);
-
-  // for...in用来迭代对象，且返回键，只能遍历可枚举属性和自有和原型属性
-  for (const key in obj) {
-    console.log("for...in", key);
-  }
-
-  // for...of用来迭代可迭代对象，且返回值，只遍历迭代器提供的值。
-  for (const value of map) {
-    console.log("for...of", value);
+  for (const key in object1) {
+    console.log(key);
   }
 }
-fn1();
+forin();
+
+/* for...of循环迭代对象的迭代器，且有一定顺序 */
+function forof() {
+  const map1 = new Map([
+    ["key1", 1],
+    ["key2", 2],
+    ["key3", 3],
+  ]);
+
+  for (const value of map1) {
+    console.log(value);
+  }
+}
+forof();
